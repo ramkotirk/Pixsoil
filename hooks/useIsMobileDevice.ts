86c238c1556d36394e3df33514d88efbac6d995c
RK <vtu23641@veltech.edu.in>
2024-09-11 00:20:26 +0530

    Delete hooks/useIsMobileDevice.ts

--- hooks/useIsMobileDevice.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export default function useIsMobileDevice() {
-    const [isMobileDevice, setIsMobileDevice] = useState(true);
-    useEffect(() => {
-        setIsMobileDevice(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-    }, []);
-    return isMobileDevice;
-}
